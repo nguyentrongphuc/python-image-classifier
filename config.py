5ad423b9929d0ce9a8f81e10c7b39aea8190bb09
Phuc Nguyen <mr.nguyentrongphuc@gmail.com>
2023-12-30 00:32:24 -0800

    modify the pth file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-MODEL_FILE = 'image-classifier-dataset1.pth'
+MODEL_FILE = 'image-classifier-dataset.pth'
 
 MEAN = [0.485, 0.456, 0.406]
 
